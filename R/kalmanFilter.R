ac2c1578d4420bfd9619b3a40950bc7af053509c
Christian Haug-Johansen <christian.hj@gmail.com>
2016-05-15 14:31:00 -0400

    correctly deals with vector inputs instead of thin matrix and fix dimensions to match dk

--- R/kalmanFilter.R
+++ R/kalmanFilter.R
@@ -6,31 +6,51 @@
 # \alpha_1 \sim N(a1, P1)
 
 kfilter <- function(yt, Z, A, R, H, Q, a1, P1) {
-  m <- length(a1)
-  n <- ifelse(is.matrix(yt), nrow(yt), length(yt))
+  # Need to convert any vectors to matrices to treat them uniformly
+  yt <- as.matrix(yt)
+  a1 <- as.matrix(a1)
+  Q <- as.matrix(Q)
+
+  # A few useful sizes
+  m <- nrow(a1)
+  n <- nrow(yt)
+  p <- ncol(yt)
+  r <- ncol(R)
+
+  # For the ones that aren't matrices, this is our best guess at their size
+  if (!is.matrix(Z)) dim(Z) <- c(p, m)
+  if (!is.matrix(Z)) dim(A) <- c(m, m)
+  if (!is.matrix(Z)) dim(R) <- c(m, r)
+  if (!is.matrix(Z)) dim(H) <- c(p, p)
+  if (!is.matrix(Z)) dim(P1) <- c(m, m)
+
+  # Check if all the dimensions are coherent
+  stopifnot(nrow(Z) == p, ncol(H) == p, nrow(H) == p)
+  stopifnot(ncol(Z) == m, nrow(A) == m, ncol(A) == m, nrow(R) == m, nrow(P1) == m, ncol(P1) == m)
+  stopifnot(nrow(Q) == r, ncol(Q) == r)
 
   # Forecast distribution
-  at <- matrix(nrow = n + 1, ncol = m)
+  at <- matrix(nrow = m, ncol = n + 1)
   Pt <- array(dim = c(m, m, n + 1))
 
   # Filtered distribution
-  att <- matrix(nrow = n, ncol = m)
+  att <- matrix(nrow = m, ncol = n)
   Ptt <- array(dim = c(m, m, n))
 
   # Initialization
-  at[1, ] <- a1
+  at[, 1] <- a1
   Pt[, , 1] <- P1
 
   for (t in 1:n) {
-    vt <- yt[t] - Z %*% at[t, ]
+    vt <- yt[t, ] - Z %*% at[, t]
     Ft <- Z %*% Pt[, , t] %*% t(Z) + H
 
     # Filtering step
-    att[t, ] <- at[t, ] + Pt[, , t] %*% t(Z) %*% solve(Ft) %*% vt
+    att[, t] <- at[, t] + Pt[, , t] %*% t(Z) %*% solve(Ft) %*% vt
     Ptt[, , t] <- Pt[, , t] - Pt[, , t] %*% t(Z) %*% solve(Ft) %*% Z %*% Pt[, , t]
 
     # Forecasting step
-    at[t+1, ] <- A %*% att[t, ]
+    at[, t+1] <- A %*% att[, t]
     Pt[, , t+1] <- A %*% Ptt[, , t] %*% t(A) + R %*% Q %*% t(R)
   }
 
